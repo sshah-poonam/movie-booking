af41730ac0d4b171b9cf05dda6d50d4c90bdbcc2
sshah-poonam <ppoonam.shah810@gmail.com>
2024-03-21 19:25:11 +0530

    update logic for the available_seats_count as per the show_timings

--- ticket_booking_system.rb
+++ ticket_booking_system.rb
@@ -9,8 +9,20 @@ class Movie
     @seats = Array.new(total_seats, false) # false means available seat
   end
 
-  def available_seats_count
-    @seats.count(false)
+  def available_seats_count(show_time)
+    show_index = @show_timings.index(show_time)
+
+    if show_index
+      # calculate the range of seats for the show
+      show_start = show_index * @total_seats / @show_timings.length
+      show_end = (show_index + 1) * @total_seats / @show_timings.length
+
+      available_seats = @seats[show_start...show_end].count(false) # count of available seats
+
+      return available_seats
+    else
+      0
+    end
   end
 
   def reserve_seat(show_time, number_tickets)
@@ -18,7 +30,7 @@ class Movie
       return "Invalid show time."
     end
 
-    available_seats = available_seats_count
+    available_seats = available_seats_count(show_time)
 
     if number_tickets > available_seats
       return "Sorry, only #{available_seats} seat(s) available for #{title} - #{show_time}."
@@ -37,7 +49,7 @@ class Movie
   def display_status
     puts "Movie: #{title} (Genre: #{genre})"
     @show_timings.each do |show_time|
-      available_seats = available_seats_count
+      available_seats = available_seats_count(show_time)
       puts "  Show: #{show_time}, Total Seats: #{@total_seats}, Available Seats: #{available_seats}"
     end
     puts "-----------------------------------------"
